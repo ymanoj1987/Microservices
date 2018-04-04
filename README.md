15bf2c4c0774c477b98511f23bd73d0446c51308
ymanoj1987 <ymanoj.se@gmail.com>
2018-04-04 00:29:08 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MicroServices
+Creating MicroServices to run on diffrent ports and deploying all these microservices in Tomcat Server.
---
+++ src/main/java/com/assignment/three/MicrosevicesApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.three;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosevicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosevicesApplication.class, args);
+	}
+}
